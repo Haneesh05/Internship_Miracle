a6126fd581f94bac7932680d2d534c9f353ad31e
Haneesh Bandaru <97587849+Haneesh05@users.noreply.github.com>
2023-06-14 07:21:35 +0530

    Update JPAService.java

--- src/internJPA/src/main/java/com/example/internJPA/Resources/JPAService.java
+++ src/internJPA/src/main/java/com/example/internJPA/Resources/JPAService.java
@@ -12,32 +12,47 @@ public class JPAService {
     @Autowired
     JPARepo js;
 
-//this method inserts data into DB 
+    //this method inserts data into DB
     public Employee insertData(Employee e) {
         return js.save(e);
     }
 
-//this method brings all the data from DB of emp table
+    //this method brings all the data from DB of emp table
     public List<Employee> getData() {
         return js.findAll();
     }
-//this method brings the data of employee from DB from a particular ID
+    //this method brings the data of employee from DB from a particular ID
     public Optional<Employee> getDataById(long id) {
         return js.findById(id);
     }
 
-//this method brings the Username from DB of a particular ID	
-    public String getNameById(long id) {
+    //this method brings the Username from DB of a particular ID
+    public String getUserNameById(long id) {
         return js.getUsername(id);
     }
 
-//this method brings the Phone number from DB of a particular ID
+    //this method brings the Phone number from DB of a particular ID
     public String getPhoneNumberById(long id) {
         return js.getPhonenumber(id);
     }
 
-//this method brings the email id from DB of a particular ID	
-    public String getEmailIdById(long id) {  
+    //this method brings the email id from DB of a particular ID
+    public String getEmailIdById(long id) {
         return js.getEmailId(id);
+    }
+    
+    //this method checks whether usename exists in DataBase
+    public boolean isUsernameExists(String username) {
+        return js.existsByUsername(username);
+    }
+
+    //this method checks whether phonenumber exists in DataBase
+    public boolean isPhoneNumberExists(String phonenumber) {
+        return js.existsByPhonenumber(phonenumber);
+    }
+
+    //this method checks whether emailId exists in DataBase
+    public boolean isEmailIdExists(String email) {
+        return js.existsByEmailId(email);
     }
 }
